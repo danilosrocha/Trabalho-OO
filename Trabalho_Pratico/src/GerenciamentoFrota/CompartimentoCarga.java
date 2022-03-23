896672dbfb29efc146d7627de6198ca42e9e6454
Danilo Rocha <danilosilvasrocha@gmail.com>
2022-03-23 16:09:21 -0300

    Criação do package gerenciamento de frota

---
+++ Trabalho_Pratico/src/GerenciamentoFrota/CompartimentoCarga.java
@@ -0,0 +1,21 @@
+package GerenciamentoFrota;
+
+public class CompartimentoCarga {
+	private String tipoCompartimento;
+	private float volume;
+	
+	public String getTipoCompartimento() {
+		return tipoCompartimento;
+	}
+	public void setTipoCompartimento(String tipoCompartimento) {
+		this.tipoCompartimento = tipoCompartimento;
+	}
+	public float getVolume() {
+		return volume;
+	}
+	public void setVolume(float volume) {
+		this.volume = volume;
+	}
+	
+	
+}
