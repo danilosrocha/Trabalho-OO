fcd22443b25b07e90e0d03fdbe34b8556c03a705
Danilo Rocha <danilosilvasrocha@gmail.com>
2022-03-23 17:48:46 -0300

    Atualização de métodos construtores

--- Trabalho_Pratico/src/GerenciamentoFrota/VeiculosUtilitarios.java
+++ Trabalho_Pratico/src/GerenciamentoFrota/VeiculosUtilitarios.java
@@ -4,6 +4,12 @@ public class VeiculosUtilitarios extends Veiculo{
 	private String tipoUtilitario;
 	private String itensOpcionais;
 	
+	public VeiculosUtilitarios(String marca, String modelo, String anoFabricacao, String numeroFabricacao,
+			float capacidadeTanque, String tipoUtilitario, String itensOpcionais) {
+		super(marca, modelo, anoFabricacao, numeroFabricacao, capacidadeTanque);
+		this.tipoUtilitario = tipoUtilitario;
+		this.itensOpcionais = itensOpcionais;
+	}
 	public String getTipoUtilitario() {
 		return tipoUtilitario;
 	}
